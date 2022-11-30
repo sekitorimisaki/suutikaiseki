6f467b9379a592eb95fa6f967d0a3993a1be6cd2
sekitorimisaki <s21c1069sh@s.chibakoudai.jp>
2022-11-30 14:55:46 +0900

    2問目修正

--- 2_repo2.cpp
+++ 2_repo2.cpp
@@ -1,28 +1,43 @@
 #include <iostream>
+#include <iomanip>
 struct alphabet_count
 {
     int alphabet;
     int count;
 };
-
+int count_letter(FILE *fp)
+{
+    int chr, count = 0;
+    while ((chr = fgetc(fp)) != EOF)
+    {
+        if (isalpha(chr) == 0)
+            count++;
+    }
+    return count;
+}
 int main()
 {
     FILE *sort;
     alphabet_count character[100];
     int chr, i = 0;
-    char read, write;
+    double number_file;
     char fname[] = "sort.txt";
     char fna[] = "2_result.txt";
     system("sort -f write.txt > sort.txt");
+    sort = fopen(fname, "r");
+    number_file = count_letter(sort);
+    fclose(sort);
     sort = fopen(fname, "r");
     character[0].alphabet = 'a';
     character[0].count = 0;
     while ((chr = fgetc(sort)) != EOF)
     {
-        if(chr == '\n'){
+        if (chr == '\n')
+        {
             continue;
         }
-        if(chr < 'a')chr=chr+32;
+        if (chr < 'a')
+            chr = chr + 32;
         if (chr == character[i].alphabet)
         {
             character[i].count++;
@@ -34,8 +49,9 @@ int main()
             character[i].count = 1;
         }
     }
-    for(int i = 0;i<26;i++){
-        std::cout << char(character[i].alphabet) << " : " << character[i].count << std::endl;
+    for (int i = 0; i < 26; i++)
+    {
+        std::cout << char(character[i].alphabet) << " : " << character[i].count << "回 (確率:"<<  std::setprecision(2)<< character[i].count/number_file*100 << "%)"<< std::endl;
     }
     fclose(sort);
 }
