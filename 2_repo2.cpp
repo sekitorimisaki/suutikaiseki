7f6b227a9cdd8cfaaddf7354d3a8a3470e3b753b
sekitorimisaki <s21c1069sh@s.chibakoudai.jp>
2022-11-30 11:09:42 +0900

    問題2のプログラム

---
+++ 2_repo2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+struct alphabet_count
+{
+    int alphabet;
+    int count;
+};
+
+int main()
+{
+    FILE *sort, *hoge;
+    alphabet_count character[100];
+    int chr, i = 0;
+    char read, write;
+    char fname[] = "sort.txt";
+    char fna[] = "hoge.txt";
+    system("sort -f write.txt > sort.txt");
+    sort = fopen(fname, "r");
+    hoge = fopen(fna, "w");
+    character[0].alphabet = 'a';
+    character[0].count = 0;
+    while ((chr = fgetc(sort)) != EOF)
+    {
+        if(chr == '\n'){
+            continue;
+        }
+        if(chr < 'a')chr=chr+32;
+        if (chr == character[i].alphabet)
+        {
+            character[i].count++;
+        }
+        else
+        {
+            i++;
+            character[i].alphabet = chr;
+            character[i].count = 1;
+        }
+    }
+    for(int i = 0;i<26;i++){
+        std::cout << char(character[i].alphabet) << " : " << character[i].count << std::endl;
+    }
+    fclose(sort);
+    fclose(hoge);
+}
